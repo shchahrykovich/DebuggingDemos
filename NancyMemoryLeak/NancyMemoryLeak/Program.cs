b7ab2df8ada87cc977aa036537f190da8c804dd2
Sergey Shchegrikovich <shchegrikovich@gmail.com>
2016-02-09 20:51:53 +0300

    Initial commit

---
+++ NancyMemoryLeak/NancyMemoryLeak/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using Nancy.Hosting.Self;
+
+namespace NancyMemoryLeak
+{
+    static class Program
+    {
+        private static NancyHost _nancyHost;
+        private static readonly CancellationTokenSource Source = new CancellationTokenSource();
+
+        static void Main(string[] args)
+        {
+            Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                Source.Cancel();
+            };
+
+            _nancyHost = new NancyHost(new Uri("http://localhost:5000"));
+            _nancyHost.Start();
+
+            WaitHandle.WaitAll(new[] {Source.Token.WaitHandle });
+        }
+    }
+}
---
+++ NancyMemoryLeak/Readme.md
@@ -0,0 +1,1 @@
+https://lowleveldesign.wordpress.com/2015/11/30/catch-in-cancellationtokensource/
